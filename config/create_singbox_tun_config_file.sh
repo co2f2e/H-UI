57d827fba787a1c2f068953070e0ed61d2fad0fc
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-12-18 10:58:28 +0800

    适用于SingBox1.11客户端

--- config/create_singbox_tun_config_file.sh
+++ config/create_singbox_tun_config_file.sh
@@ -4,7 +4,8 @@ OUTPUT_FILE="./singbox_tun.json"
 while true; do
 read -p "请输入Hysteria2节点的数量: " SERVER_COUNT
 if ! [[ "$SERVER_COUNT" =~ ^[1-9]+$ ]]; then
-  echo "请输入1~9有效的数字"
+  echo "输入有误，请输入1~9有效的数字"
+  echo
 else
   break
 fi
@@ -12,7 +13,8 @@ done
 while true; do
 read -p "请设置上传速度(mbps): " UP_MBPS
 if ! [[ "$UP_MBPS" =~ ^[1-9999]+$ ]]; then
-  echo "请输入1~9999有效的数字"
+  echo "输入有误，请输入1~9999有效的数字"
+  echo
 else
   break
 fi
@@ -20,7 +22,8 @@ done
 while true; do
 read -p "请设置下载速度(mbps): " DOWN_MBPS
 if ! [[ "$DOWN_MBPS" =~ ^[1-9999]+$ ]]; then
-  echo "请输入1~9999有效的数字"
+  echo "输入有误，请输入1~9999有效的数字"
+  echo
 else
   break
 fi
