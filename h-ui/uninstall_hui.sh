dbd6e0d300bbb4bf7316e1f4cf63476f1a653471
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-12-21 00:25:43 +0800

    Update uninstall_hui.sh

--- h-ui/uninstall_hui.sh
+++ h-ui/uninstall_hui.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
 clear
-
 systemctl stop h-ui
-
+RESTART_HUI="/usr/local/h-ui/restart-hui.sh"
+echo "正在删除 crontab 中的定时任务..."
+(crontab -l 2>/dev/null | grep -v "$RESTART_HUI") | crontab -
 rm -rf /etc/systemd/system/h-ui.service /usr/local/h-ui/
-
 echo "卸载成功!!!"
