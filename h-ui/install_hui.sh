7928a2ed7ddff8f46ae3f81727f7b4ab237b5202
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-12-21 00:17:50 +0800

    Update install_hui.sh

--- h-ui/install_hui.sh
+++ h-ui/install_hui.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 clear
 mkdir -p /usr/local/h-ui/
 curl -fsSL https://github.com/jonssonyan/h-ui/releases/latest/download/h-ui-linux-amd64 -o /usr/local/h-ui/h-ui && chmod +x /usr/local/h-ui/h-ui
@@ -8,10 +7,24 @@ sed -i "s|^ExecStart=.*|ExecStart=/usr/local/h-ui/h-ui -p 6812|" "/etc/systemd/s
 systemctl daemon-reload
 systemctl enable h-ui
 systemctl restart h-ui
-
 echo
 echo "登录方式：IP:PORT"
 echo "面板端口：6812"
 echo "用户名：sysadmin"
 echo "密码：sysadmin"
 echo
+if ! command -v crontab &> /dev/null; then
+  echo "cron 未安装，正在安装 cron..."
+  sudo apt update > /dev/null 2>&1
+  sudo apt install -y cron > /dev/null 2>&1
+  sudo systemctl enable cron
+  sudo systemctl start cron
+fi
+RESTART_HUI="/usr/local/h-ui/restart-h-ui.sh"
+cat <<EOF | sudo tee $RESTART_HUI > /dev/null
+#!/bin/bash
+sudo systemctl restart h-ui
+EOF
+sudo chmod +x $RESTART_HUI
+(crontab -l 2>/dev/null; echo "0 3 * * * $RESTART_HUI") | crontab -
+echo "h-ui服务安装完成，定时任务已设置为每天凌晨 3 点重启服务!!!"
